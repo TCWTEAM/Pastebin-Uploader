8eb626be677105e6780a3322fba1aed93fc7d868
TCWTEAM <admin@tcwsite.us>
2017-07-09 10:28:48 -0400

    added more customizability

--- PasteBin Uploader/Form1.Designer.vb
+++ PasteBin Uploader/Form1.Designer.vb
@@ -23,10 +23,12 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.MephTheme1 = New PasteBin_Uploader.MephTheme()
+        Me.cmbEXP = New PasteBin_Uploader.MephComboBox()
+        Me.txtName = New PasteBin_Uploader.MephTextBox()
+        Me.txtUrl = New PasteBin_Uploader.MephTextBox()
+        Me.btnUpload = New PasteBin_Uploader.MephButton()
         Me.btnBrowse = New PasteBin_Uploader.MephButton()
         Me.txtPath = New PasteBin_Uploader.MephTextBox()
-        Me.btnUpload = New PasteBin_Uploader.MephButton()
-        Me.txtUrl = New PasteBin_Uploader.MephTextBox()
         Me.MephTheme1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -34,6 +36,8 @@ Partial Class Form1
         '
         Me.MephTheme1.AccentColor = System.Drawing.Color.LimeGreen
         Me.MephTheme1.BackColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
+        Me.MephTheme1.Controls.Add(Me.cmbEXP)
+        Me.MephTheme1.Controls.Add(Me.txtName)
         Me.MephTheme1.Controls.Add(Me.txtUrl)
         Me.MephTheme1.Controls.Add(Me.btnUpload)
         Me.MephTheme1.Controls.Add(Me.btnBrowse)
@@ -41,51 +45,48 @@ Partial Class Form1
         Me.MephTheme1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.MephTheme1.Location = New System.Drawing.Point(0, 0)
         Me.MephTheme1.Name = "MephTheme1"
-        Me.MephTheme1.Size = New System.Drawing.Size(339, 177)
+        Me.MephTheme1.Size = New System.Drawing.Size(339, 224)
         Me.MephTheme1.SubHeader = "By TCWLightning"
         Me.MephTheme1.TabIndex = 0
         Me.MephTheme1.Text = "Pastebin Uploader"
         '
-        'btnBrowse
+        'cmbEXP
         '
-        Me.btnBrowse.BackColor = System.Drawing.Color.Transparent
-        Me.btnBrowse.ForeColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
-        Me.btnBrowse.Location = New System.Drawing.Point(292, 77)
-        Me.btnBrowse.Name = "btnBrowse"
-        Me.btnBrowse.Size = New System.Drawing.Size(35, 23)
-        Me.btnBrowse.TabIndex = 1
-        Me.btnBrowse.Text = "..."
+        Me.cmbEXP.BackColor = System.Drawing.Color.Transparent
+        Me.cmbEXP.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed
+        Me.cmbEXP.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbEXP.Font = New System.Drawing.Font("Verdana", 8.0!)
+        Me.cmbEXP.ForeColor = System.Drawing.Color.LimeGreen
+        Me.cmbEXP.FormattingEnabled = True
+        Me.cmbEXP.ItemHeight = 16
+        Me.cmbEXP.ItemHighlightColor = System.Drawing.Color.FromArgb(CType(CType(55, Byte), Integer), CType(CType(55, Byte), Integer), CType(CType(55, Byte), Integer))
+        Me.cmbEXP.Items.AddRange(New Object() {"Select Expire Date", "Never ", "10 Minutes", "1 Hour", "1 Day", "1 Week", "2 Weeks ", "1 Month"})
+        Me.cmbEXP.Location = New System.Drawing.Point(69, 127)
+        Me.cmbEXP.Name = "cmbEXP"
+        Me.cmbEXP.Size = New System.Drawing.Size(191, 22)
+        Me.cmbEXP.StartIndex = 0
+        Me.cmbEXP.TabIndex = 5
         '
-        'txtPath
+        'txtName
         '
-        Me.txtPath.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
-        Me.txtPath.ForeColor = System.Drawing.Color.LimeGreen
-        Me.txtPath.Location = New System.Drawing.Point(12, 76)
-        Me.txtPath.MaxLength = 32767
-        Me.txtPath.MultiLine = False
-        Me.txtPath.Name = "txtPath"
-        Me.txtPath.Size = New System.Drawing.Size(274, 24)
-        Me.txtPath.TabIndex = 0
-        Me.txtPath.Text = ".txt Path"
-        Me.txtPath.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
-        Me.txtPath.UseSystemPasswordChar = False
-        Me.txtPath.WordWrap = False
-        '
-        'btnUpload
-        '
-        Me.btnUpload.BackColor = System.Drawing.Color.Transparent
-        Me.btnUpload.ForeColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
-        Me.btnUpload.Location = New System.Drawing.Point(71, 136)
-        Me.btnUpload.Name = "btnUpload"
-        Me.btnUpload.Size = New System.Drawing.Size(191, 23)
-        Me.btnUpload.TabIndex = 2
-        Me.btnUpload.Text = "Upload"
+        Me.txtName.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.txtName.ForeColor = System.Drawing.Color.LimeGreen
+        Me.txtName.Location = New System.Drawing.Point(34, 97)
+        Me.txtName.MaxLength = 32767
+        Me.txtName.MultiLine = False
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(274, 24)
+        Me.txtName.TabIndex = 4
+        Me.txtName.Text = "Name"
+        Me.txtName.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.txtName.UseSystemPasswordChar = False
+        Me.txtName.WordWrap = False
         '
         'txtUrl
         '
         Me.txtUrl.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
         Me.txtUrl.ForeColor = System.Drawing.Color.LimeGreen
-        Me.txtUrl.Location = New System.Drawing.Point(32, 106)
+        Me.txtUrl.Location = New System.Drawing.Point(34, 155)
         Me.txtUrl.MaxLength = 32767
         Me.txtUrl.MultiLine = False
         Me.txtUrl.Name = "txtUrl"
@@ -96,11 +97,46 @@ Partial Class Form1
         Me.txtUrl.UseSystemPasswordChar = False
         Me.txtUrl.WordWrap = False
         '
+        'btnUpload
+        '
+        Me.btnUpload.BackColor = System.Drawing.Color.Transparent
+        Me.btnUpload.ForeColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
+        Me.btnUpload.Location = New System.Drawing.Point(69, 185)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(191, 23)
+        Me.btnUpload.TabIndex = 2
+        Me.btnUpload.Text = "Upload"
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.BackColor = System.Drawing.Color.Transparent
+        Me.btnBrowse.ForeColor = System.Drawing.Color.FromArgb(CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer), CType(CType(205, Byte), Integer))
+        Me.btnBrowse.Location = New System.Drawing.Point(292, 67)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(35, 23)
+        Me.btnBrowse.TabIndex = 1
+        Me.btnBrowse.Text = "..."
+        '
+        'txtPath
+        '
+        Me.txtPath.BackColor = System.Drawing.Color.FromArgb(CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer), CType(CType(50, Byte), Integer))
+        Me.txtPath.ForeColor = System.Drawing.Color.LimeGreen
+        Me.txtPath.Location = New System.Drawing.Point(12, 67)
+        Me.txtPath.MaxLength = 32767
+        Me.txtPath.MultiLine = False
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.Size = New System.Drawing.Size(274, 24)
+        Me.txtPath.TabIndex = 0
+        Me.txtPath.Text = ".txt Path"
+        Me.txtPath.TextAlignment = System.Windows.Forms.HorizontalAlignment.Left
+        Me.txtPath.UseSystemPasswordChar = False
+        Me.txtPath.WordWrap = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(339, 177)
+        Me.ClientSize = New System.Drawing.Size(339, 224)
         Me.Controls.Add(Me.MephTheme1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Form1"
@@ -116,4 +152,6 @@ Partial Class Form1
     Friend WithEvents txtPath As MephTextBox
     Friend WithEvents txtUrl As MephTextBox
     Friend WithEvents btnUpload As MephButton
+    Friend WithEvents cmbEXP As MephComboBox
+    Friend WithEvents txtName As MephTextBox
 End Class
--- PasteBin Uploader/Form1.vb
+++ PasteBin Uploader/Form1.vb
@@ -21,7 +21,7 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
+        cmbEXP.SelectedItem = "Choose Expire Date"
     End Sub
 
     Private Sub btnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
@@ -29,15 +29,33 @@ Public Class Form1
     End Sub
 
     Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        Dim expire As String
+        If cmbEXP.SelectedItem = "Never" Then
+            expire = "N"
+        ElseIf cmbEXP.SelectedItem = "10 Minutes" Then
+            expire = "10M"
+        ElseIf cmbEXP.SelectedItem = "1 Hour" Then
+            expire = "1H"
+        ElseIf cmbEXP.SelectedItem = "1 Day" Then
+            expire = "1D"
+        ElseIf cmbEXP.SelectedItem = "1 Week" Then
+            expire = "1W"
+        ElseIf cmbEXP.SelectedItem = "2 Weeks" Then
+            expire = "2W"
+        ElseIf cmbEXP.SelectedItem = "1 Month" Then
+            expire = "1M"
+        Else
+            MessageBox.Show("Invalid expire date", "Pastebin Uploader", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
         Try
             Dim filepath As String = txtPath.Text
             Dim sr As New StreamReader(filepath)
             Dim content As String = sr.ReadToEnd()
             Dim data As New System.Collections.Specialized.NameValueCollection()
-            data("api_paste_name") = "PasteBin Uploader"
-            data("api_paste_expire_date") = "N"
+            data("api_paste_name") = txtName.Text
+            data("api_paste_expire_date") = expire
             data("api_paste_code") = content
-            data("api_dev_key") = `"DEV KEY"
+            data("api_dev_key") = "DEV KEY"
             data("api_option") = "paste"
             Dim bytes As Byte() = wclient.UploadValues("https://pastebin.com/api/api_post.php", data)
             Dim response As String

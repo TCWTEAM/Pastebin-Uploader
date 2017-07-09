0dd58427c2b8f70d2243df0c3e8e67e9f3c50774
TCWTEAM <admin@tcwsite.us>
2017-07-08 22:14:39 -0400

    Added exception

--- PasteBin Uploader/Form1.vb
+++ PasteBin Uploader/Form1.vb
@@ -29,29 +29,35 @@ Public Class Form1
     End Sub
 
     Private Sub btnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
-        Dim filepath As String = txtPath.Text
-        Dim sr As New StreamReader(filepath)
-        Dim content As String = sr.ReadToEnd()
-        Dim data As New System.Collections.Specialized.NameValueCollection()
-        data("api_paste_name") = "PasteBin Uploader"
-        data("api_paste_expire_date") = "N"
-        data("api_paste_code") = content
-        data("api_dev_key") = `"PASTEBIN DEVKEY HERE"
-        data("api_option") = "paste"
-        Dim bytes As Byte() = wclient.UploadValues("https://pastebin.com/api/api_post.php", data)
-        Dim response As String
-        Using ms As New MemoryStream(bytes)
-            Using reader As New StreamReader(ms)
-                response = reader.ReadToEnd()
+        Try
+            Dim filepath As String = txtPath.Text
+            Dim sr As New StreamReader(filepath)
+            Dim content As String = sr.ReadToEnd()
+            Dim data As New System.Collections.Specialized.NameValueCollection()
+            data("api_paste_name") = "PasteBin Uploader"
+            data("api_paste_expire_date") = "N"
+            data("api_paste_code") = content
+            data("api_dev_key") = `"DEV KEY"
+            data("api_option") = "paste"
+            Dim bytes As Byte() = wclient.UploadValues("https://pastebin.com/api/api_post.php", data)
+            Dim response As String
+            Using ms As New MemoryStream(bytes)
+                Using reader As New StreamReader(ms)
+                    response = reader.ReadToEnd()
+                End Using
             End Using
-        End Using
-        If response.StartsWith("Bad API Request") Then
-            MessageBox.Show("Error uploading", "Pastebin Uploader", MessageBoxButtons.OK, MessageBoxIcon.Error)
-        Else
-            txtUrl.Text = response
-            Clipboard.Clear()
-            Clipboard.SetText(response)
-            MessageBox.Show("Successfuly Uploaded, the link has been copied to clipboard", "Pastebin Uploader", MessageBoxButtons.OK, MessageBoxIcon.Information)
-        End If
+            If response.StartsWith("Bad API Request") Then
+                MessageBox.Show("Error uploading", "Pastebin Uploader", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                txtUrl.Text = response
+                Clipboard.Clear()
+                Clipboard.SetText(response)
+                MessageBox.Show("Successfuly Uploaded, the link has been copied to clipboard", "Pastebin Uploader", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error", "Pastebin Uploader", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End Try
     End Sub
 End Class
